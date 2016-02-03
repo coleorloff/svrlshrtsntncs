22bb39cec50dbb6ff6f5027860eb0d92c693d744
C013 <co790@nyu.edu>
2016-02-03 10:17:19 -0500

    input fixed at bottom.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 var text_input = document.getElementById('text_input');
-var print_here = document.getElementById('print_here');
-
-document.onkeyup = function(){
-	mirror();
-};
+var ENTER = 13;
 
 document.onkeypress = function(e){
-	if (e.keyCode == 13){
+	if (e.keyCode == ENTER){
 		newLine();
 		clearInput();
 	} else if (e.keyCode !== null){
@@ -14,24 +10,12 @@ document.onkeypress = function(e){
 	}
 };
 
-var getInput = function(){
-	console.log("number:" + text_input.valueOf());
-	document.getElementsByClassName("input__label--hoshi").stlye.property.width = text_input.valueOf();
-}
-
 var newLine = function(){
 	console.log("new line")
 	var para = document.createElement('P');
 	var t = document.createTextNode(text_input.value);
-	
 	para.appendChild(t);
 	document.getElementById('save_here').appendChild(para);
-}
-
-var mirror = function(){
-	console.log("mirror")
-	console.log(text_input.value);
-	print_here.innerHTML = text_input.value;
 }
 
 var clearInput = function(){
