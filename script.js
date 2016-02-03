6daf87eaea3d012bdfa0a933439342e489a3ce61
C013 <co790@nyu.edu>
2016-02-03 08:30:31 -0500

    playing with css

--- script.js
+++ script.js
@@ -14,19 +14,28 @@ document.onkeypress = function(e){
 	}
 };
 
+var getInput = function(){
+	console.log("number:" + text_input.valueOf());
+	document.getElementsByClassName("input__label--hoshi").stlye.property.width = text_input.valueOf();
+}
+
 var newLine = function(){
+	console.log("new line")
 	var para = document.createElement('P');
 	var t = document.createTextNode(text_input.value);
+	
 	para.appendChild(t);
 	document.getElementById('save_here').appendChild(para);
 }
 
 var mirror = function(){
+	console.log("mirror")
 	console.log(text_input.value);
 	print_here.innerHTML = text_input.value;
 }
 
 var clearInput = function(){
+	console.log("clear input")
 	document.getElementById('text_input').value = "";
 };
 
